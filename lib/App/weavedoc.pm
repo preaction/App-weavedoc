5fb4cd18d212efa03b3944798b3c1a1d9e2c6b01
Doug Bell <doug@preaction.me>
2017-11-03 18:00:11 -0500

    add weavedoc script

--- lib/App/weavedoc.pm
+++ lib/App/weavedoc.pm
@@ -1,12 +1,21 @@
 package App::weavedoc;
 our $VERSION = '0.001';
-# ABSTRACT: Write a sentence about what it does
+# ABSTRACT: Show documentation for a module using Pod::Weaver
 
 =head1 SYNOPSIS
 
+    weavedoc [--license <license>] [--version <version>] [--author <author>] <file>
+    weavedoc -h|--help
+
 =head1 DESCRIPTION
 
+This distribution contains a command line utility to take a file
+with L<Pod::Weaver> directives and render it to the terminal like
+L<the perldoc utility|perldoc> does.
+
 =head1 SEE ALSO
+
+L<Pod::Weaver>, L<App::podweaver>, L<perldoc>
 
 =cut
 
